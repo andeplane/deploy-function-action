fb3d37336226d61a0bf7a03ee97a9a7af89a69be
Anders Hafreager <anders.hafreager@cognite.com>
2020-04-11 20:28:37 +0200

    Using repo name and sha

--- src/index.js
+++ src/index.js
@@ -13,7 +13,15 @@ const CDF_PROJECT = process.env.INPUT_CDF_PROJECT;
 const CDF_CREDENTIALS = process.env.INPUT_CDF_CREDENTIALS;
 
 const FUNCTION_PATH = process.env.INPUT_HANDLER_PATH;
-console.log("Environment variables: ", process.env)
+
+const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY;
+
+const GITHUB_EVENT_NAME = process.env.GITHUB_EVENT_NAME;
+
+const GITHUB_SHA = process.env.GITHUB_SHA.substring(0,7);
+
+const functionRefName = GITHUB_REPOSITORY+":"+GITHUB_SHA;
+
 const sdk = new CogniteClient({
   baseUrl: CDF_BASE_URL,
   appId: 'deploy-function-action',
@@ -30,21 +38,31 @@ if (!(CDF_PROJECT && CDF_CREDENTIALS)) {
 
 sdk.loginWithApiKey({ apiKey: CDF_CREDENTIALS, project: CDF_PROJECT });
 
-async function run() {
-  const user = await sdk.login.status();
-  core.debug(`Logged in as user ${user.user}`);
+async function uploadSourceCode() {
+  const fileName = functionRefName+".zip";
   await zip.addLocalFile(FUNCTION_PATH);
   const buffer = zip.toBuffer();
   const fileResponse = await sdk.files.upload(
     {
-      externalId: 'deploy-function-action-example',
-      name: 'deploy-function-action-example',
+      externalId: functionRefName,
+      name: fileName,
       mimeType: 'application/zip',
     },
     buffer,
     true,
     true
   );
+
+  return fileResponse;
+}
+
+async function deployFunction()
+
+async function run() {
+  const user = await sdk.login.status();
+  core.debug(`Logged in as user ${user.user}`);
+  const fileResponse = await uploadSourceCode();
+
   try {
     const functionResponse = await sdk.post(
       `/api/playground/projects/${CDF_PROJECT}/functions`,
